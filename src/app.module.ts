a4bcfc1edbe428c466859a31f3774a0a574af8bf
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2024-04-03 20:38:51 +0200

    moves AuthGuard to AuthModule [NOT OK]
    instantiates AuthGuard twice (in AuthModule and AppModule)
    
    Signed-off-by: Benjamin Kroeger <benjamin.kroeger@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppSecondController } from './app-second.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController, AppSecondController],
   providers: [AppService],
 })
